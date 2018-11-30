093fef0c31ba3462f478c293ce7e4c3747feba23
Stephane Balasse <stephane.balasse@qiis.fr>
2018-11-30 13:26:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# :rocket: ngx-cypress
+
+> add Cypress to the project
+
+This add-on was generated with [ngX-Rocket](https://github.com/ngx-rocket/generator-ngx-rocket/) version 5.3.0
+
+# Installation
+
+Install [ngX-Rocket CLI](https://github.com/ngx-rocket/cli) and this add-on:
+
+```sh
+npm install -g @ngx-rocket/cli ngx-cypress
+```
+
+# Usage
+
+Generate a new project and follow instructions:
+```sh
+ngx new
+```
+
+See [ngX-Rocket CLI](https://github.com/ngx-rocket/cli) for more information.
---
+++ generators/app/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const Generator = require('@ngx-rocket/core');
+const chalk = require('chalk');
+const pkg = require('../../package.json');
+
+class NgxCypressGenerator extends Generator {
+  // DO NOT add a constructor, it won't be called.
+  // Use initializing() method instead.
+  //
+  // See Yeoman's doc run loop priorities for the list of specific tasks:
+  // http://yeoman.io/authoring/running-context.html
+
+  initializing() {
+    // Setting version allows Yeoman to notify the user of updates
+    this.version = pkg.version;
+    this.log(`Using ${chalk.cyan('ngx-cypress')} ${chalk.green(this.version)}`);
+  }
+
+  beforeWriting() {
+    // Augment this generator's properties with shared properties so it can be
+    // used in templates
+    Object.assign(this.props, this.sharedProps);
+  }
+
+  end() {
+    if (this.props.addCypress && !this.updating) {
+      this.log(`- $ ${chalk.green(`${this.packageManager} run npm cypress`)}: Launch e2e Cypress`);
+    }
+  }
+}
+
+module.exports = Generator.make({
+  // Base directory of your templates
+  baseDir: __dirname,
+
+  // Your generator (optional, you can use only templates)
+  generator: NgxCypressGenerator,
+
+  // The type of your generator, can be 'client', 'server' or 'fullstack' (optional)
+  type: 'client',
+
+  // Your generator prompts (optional)
+  // See https://github.com/sboudrias/Inquirer.js#objects for details
+  prompts: [
+    {
+      type: 'confirm',
+      name: 'addCypress',
+      message: 'Do you want Cypress ?',
+      default: true
+    }
+  ]
+});
